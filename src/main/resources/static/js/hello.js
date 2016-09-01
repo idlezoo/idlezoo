27fbf1d7b035cee03b359ba9054a27a56cc72f51
Ivan Sopov <sopov.ivan@gmail.com>
2016-09-01 12:10:28 +0300

    initial commit - modified sample

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,81 @@
+angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider) {
+
+	$routeProvider.when('/', {
+		templateUrl : 'home.html',
+		controller : 'home',
+		controllerAs: 'controller'
+	}).when('/login', {
+		templateUrl : 'login.html',
+		controller : 'navigation',
+		controllerAs: 'controller'
+	}).otherwise('/');
+
+}).controller('navigation',
+
+function($rootScope, $http, $location, $route) {
+	
+	var self = this;
+
+	self.tab = function(route) {
+		return $route.current && route === $route.current.controller;
+	};
+
+	var authenticate = function(callback) {
+
+		$http.get('user').then(function(response) {
+			if (response.data.name) {
+				$rootScope.authenticated = true;
+			} else {
+				$rootScope.authenticated = false;
+			}
+			callback && callback();
+		}, function() {
+			$rootScope.authenticated = false;
+			callback && callback();
+		});
+
+	}
+
+	authenticate();
+
+	self.credentials = {};
+	self.login = function() {
+		$http.post('login', $.param(self.credentials), {
+			headers : {
+				"content-type" : "application/x-www-form-urlencoded"
+			}
+		}).then(function() {
+			authenticate(function() {
+				if ($rootScope.authenticated) {
+					console.log("Login succeeded")
+					$location.path("/");
+					self.error = false;
+					$rootScope.authenticated = true;
+				} else {
+					console.log("Login failed with redirect")
+					$location.path("/login");
+					self.error = true;
+					$rootScope.authenticated = false;
+				}
+			});
+		}, function() {
+			console.log("Login failed")
+			$location.path("/login");
+			self.error = true;
+			$rootScope.authenticated = false;
+		})
+	};
+
+	self.logout = function() {
+		$http.post('logout', {}).finally(function() {
+			$rootScope.authenticated = false;
+			$location.path("/");
+		});
+	}
+		
+}).controller('home', function($http) {
+	var self = this;
+	$http.get('/resource/').then(function(response) {
+		self.greeting = response.data;
+	})
+});

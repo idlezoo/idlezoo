d069ed7174ef5cbdf5702bc23e30539854fb4121
Ivan Sopov <sopov.ivan@gmail.com>
2016-09-08 13:07:58 +0300

    ui for temples

--- src/main/resources/static/js/home.controller.js
+++ src/main/resources/static/js/home.controller.js
@@ -1,6 +1,19 @@
-idlemage.controller('home', function($http) {
+idlemage.controller('home', function($scope, $http) {
 	var self = this;
-	$http.get('/resource/').then(function(response) {
-		self.greeting = response.data;
+	$http.get('/game/me').then(function(response) {
+		self.mage = response.data;
 	})
+	
+	
+	$scope.buy = function($buildingName) {
+		$http.get('/game/buy?building=' + $buildingName).then(function(response){
+			self.mage = response.data;
+		})
+	};
+	
+	$scope.upgrade = function($buildingName) {
+		$http.get('/game/upgrade?building=' + $buildingName).then(function(response){
+			self.mage = response.data;
+		})
+	};
 });

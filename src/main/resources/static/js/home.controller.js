46f826510dad648b006944c0ac600e9478be06e2
Ivan Sopov <sopov.ivan@gmail.com>
2016-11-04 19:57:37 +0300

    open websocket only if loggedin

--- src/main/resources/static/js/home.controller.js
+++ src/main/resources/static/js/home.controller.js
@@ -1,6 +1,11 @@
-idlemage.controller('home', function($scope, $interval, $http) {
-	
+idlemage.controller('home', function($rootScope, $scope, $interval, $http) {
+	if(!$rootScope.authenticated){
+		return;
+	}
 	var socket = new SockJS("/game/ws");
+	
+	
+	var self = this;
 	
 	socket.onopen = function () {
 		// Socket open.. start the game loop.
@@ -10,50 +15,50 @@ idlemage.controller('home', function($scope, $interval, $http) {
 			// Prevent server read timeout.
 			socket.send('ping');
 		}, 5000);
-	};
-	
-	
-	var self = this;
-	
-	
-	$scope.buy = function($buildingName) {
-		socket.send('buy/' + $buildingName);
-	};
-	
-	$scope.upgrade = function($buildingName) {
-		socket.send('upgrade/' + $buildingName);
-	};
-	
-	$scope.fight = function(){
-		socket.send('fight');
-	};
-	
-	socket.onmessage = function (message) {
-		console.log('Message from socket ' + message.data);
-		self.mage = JSON.parse(message.data);
-	};
-	
 
-	var stop;
-    // Don't start update if it is already defined
-    if ( !angular.isDefined(stop) ) {
-    	stop = $interval(function() {
-    		self.mage.mana += self.mage.manaIncome / 10;
-    		if(self.mage.waitingForFight){
-    			self.mage.championTime += 0.1;
-    		}
-    	}, 100);
-	}
+		
+		
+		$scope.buy = function($buildingName) {
+			socket.send('buy/' + $buildingName);
+		};
+		
+		$scope.upgrade = function($buildingName) {
+			socket.send('upgrade/' + $buildingName);
+		};
+		
+		$scope.fight = function(){
+			socket.send('fight');
+		};
+		
+		socket.onmessage = function (message) {
+			console.log('Message from socket ' + message.data);
+			self.mage = JSON.parse(message.data);
+		};
+		
 
-    $scope.stopUpdate = function() {
-        if (angular.isDefined(stop)) {
-        	$interval.cancel(stop);
-        	stop = undefined;
-        }
-    };
+		var stop;
+	    // Don't start update if it is already defined
+	    if ( !angular.isDefined(stop) ) {
+	    	stop = $interval(function() {
+	    		self.mage.mana += self.mage.manaIncome / 10;
+	    		if(self.mage.waitingForFight){
+	    			self.mage.championTime += 0.1;
+	    		}
+	    	}, 100);
+		}
 
-	$scope.$on('$destroy', function() {
-        $scope.stopUpdate();
-    });
+	    $scope.stopUpdate = function() {
+	        if (angular.isDefined(stop)) {
+	        	$interval.cancel(stop);
+	        	stop = undefined;
+	        }
+	    };
+
+		$scope.$on('$destroy', function() {
+			console.log('Home controller scope destory');
+	        $scope.stopUpdate();
+	    });
+	};
+
 
 });

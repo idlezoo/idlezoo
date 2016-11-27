186125440618eedc0e437aba40d64b8d83fb5554
Ivan Sopov <sopov.ivan@gmail.com>
2016-11-27 20:16:23 +0300

    use spring-session backed by redis on heroku

--- src/main/java/idlezoo/HerokuConfiguration.java
+++ src/main/java/idlezoo/HerokuConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("postgres")
+@Profile("heroku")
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
-public class PostgresConfiguration {
+public class HerokuConfiguration {
 
 }
